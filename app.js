58bb5ea0b75e217a7b1ad228052dae6701d1ec54
Md Zobaidul Islam <mdzobaidulislamkazi@gmail.com>
2023-06-01 12:18:40 +0600

    express server add and mongoss
    mongose server and express

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-const name = {
-    else: 'kazi islam kazi'
-}
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000; // Set your desired port number
+
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
+  });
+
+
+  // Example route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Additional routes and middleware can be defined here
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
